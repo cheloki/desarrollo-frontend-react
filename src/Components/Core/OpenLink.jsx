44474135db3b22cc1d0dfbebb8000288dcd4d1d6
Marcelo Guzman Morales <chelaki_7@hotmail.com>
2025-01-08 21:50:30 -0400

    imagenes linea

--- src/Components/Core/OpenLink.jsx
+++ src/Components/Core/OpenLink.jsx
@@ -3,14 +3,14 @@ const OpenLink = ({redirectURL,LogoImage}) =>{
 
     return (
       
-                <div>
+           
                             
                             <a href={redirectURL} target="_blank">
                                 <img src={LogoImage} className="logo react" alt="React logo" />
                             </a>
-                </div>
+           
         
     );
 };
 
-export default OpenLink;
+export default OpenLink; 
