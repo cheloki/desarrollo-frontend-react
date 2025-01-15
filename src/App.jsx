195a4216b50c3f5d92139dc970f760ecab1cb725
Marcelo Guzman Morales <chelaki_7@hotmail.com>
2025-01-14 20:19:38 -0400

    'Practica1'

--- src/App.jsx
+++ src/App.jsx
@@ -9,12 +9,13 @@ import './App.css'
 
 import Home from './screens/Home.jsx';
 import  Counter   from './Components/Counter/Counter.jsx';
-
+import Product from './screens/Product.jsx';
 function App() {
   return (
     <>
       <Home />
       <Counter />
+      <Product />
     </>
   )
 }
---
+++ src/screens/Product.jsx
@@ -0,0 +1,29 @@
+import React, { useState, useEffect } from 'react';
+import ProductTable from '../Components/Product/ProductTable.jsx'
+import SearchBar from '../Components/Product/Search.jsx';
+import { fetchProducts } from '../store/Product/ProductApi.jsx';
+
+const Product = () => {
+  const [products, setProducts] = useState([]);
+  const [filterText, setFilterText] = useState('');
+  const [inStockOnly, setInStockOnly] = useState(false);
+
+  useEffect(() => {
+    setProducts(fetchProducts());
+  }, []);
+
+  return (
+    <div>
+      <h1>Product List</h1>
+      <SearchBar
+        filterText={filterText}
+        inStockOnly={inStockOnly}
+        onFilterTextChange={setFilterText}
+        onInStockChange={setInStockOnly}
+      />
+      <ProductTable products={products} filterText={filterText} inStockOnly={inStockOnly} />
+    </div>
+  );
+};
+
+export default Product;
