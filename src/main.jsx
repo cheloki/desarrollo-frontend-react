f53f233929b01598fda96c64481ca72c350d2f5a
Marcelo Guzman Morales <chelaki_7@hotmail.com>
2025-01-16 20:35:19 -0400

    'adding_cicd'

--- src/main.jsx
+++ src/main.jsx
@@ -12,7 +12,8 @@ import App from './App.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+    <BrowserRouter basename="/desarrollo-frontend-react/">
+
         <App />
       </BrowserRouter>
 
